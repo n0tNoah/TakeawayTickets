3d2f08cba8d693e79f6b1e1b47f30f07041b824a
noah <you@example.com>
2021-01-03 17:29:13 +0530

    pending

--- santorini/data_models/ticket.py
+++ santorini/data_models/ticket.py
@@ -5,10 +5,16 @@ from django.forms import ModelForm
 class ticket(models.Model):
     property_name = models.ForeignKey(Propertree,on_delete=models.PROTECT)
     ticket_id = models.UUIDField(primary_key=True,default = uuid.uuid4,editable = False)
+    create_date = models.DateTimeField(default,editable=False)
     description = models.TextField()
+
 
     def __str__(self):
         return f"{self.property_name} {self.description[:10]}... "
+
+class Image(models.Model):
+   ticket = models.Foreignkey(ticket)
+   image = models.ImageField(upload_to='user_images')
 
 class ticketForm(ModelForm):
     class Meta:
